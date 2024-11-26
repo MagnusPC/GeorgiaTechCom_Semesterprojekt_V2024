4cb3301799477e6ff5ee62c7804e92d68f22db8b
bjoernUCN <103989859+bjoernUCN@users.noreply.github.com>
2024-11-26 01:37:44 +0100

    initial commit for book search

---
+++ GeorgiaTechLib/Webshop.BookSearch.Service/Program.cs
@@ -0,0 +1,26 @@
+﻿using Webshop.Tools.Messaging;
+
+
+namespace Webshop.BookSearch.Service
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+
+            string queueName = "PostSearch";
+
+            var consumer = new Consumer<string>(
+                _onConsume: message =>
+                {
+                    Console.WriteLine($" [x] Received: {message.Content} with ID: {message.CorrelationId}");
+                },
+                _queue: queueName
+            );
+
+            await consumer.Connect();
+            
+
+        }
+    }
+}
