763cc9c18b804ce6281fcc1c6fb8750025bfb987
MagnusPC <103988876+MagnusPC@users.noreply.github.com>
2024-11-28 15:47:20 +0100

    some setup
    nb some line break warnings

--- GeorgiaTechLib/Webshop.Search.Persisstence/ISearchRepository.cs
+++ GeorgiaTechLib/Webshop.Search.Persisstence/ISearchRepository.cs
@@ -3,10 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Webshop.Application.Contracts.Persistence;
+using Webshop.Catalog.Domain.AggregateRoots;
+using Webshop.Domain.Common;
 
 namespace Webshop.Search.Persistence
 {
-    internal interface ISearchRepository
+    internal interface ISearchRepository : IRepository<Product> //SearchTerm?
     {
+        Task<IEnumerable<Product>> GetAllFromCategory(int categoryId);
+
+
     }
 }
