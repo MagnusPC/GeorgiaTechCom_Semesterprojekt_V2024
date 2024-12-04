4ba1ac467a845eb6e57e312a8ecbbc8c09b9637b
bjoernUCN <37444348+Bjoe3041@users.noreply.github.com>
2024-12-04 10:18:09 +0100

    a

---
+++ GeorgiaTechLib/DataWrappers/CRUDWrapper.cs
@@ -0,0 +1,39 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Webshop.Tools.DataWrappers
+{
+    public class CRUDWrapper<T>
+    {
+        public T Data { get; set; }
+        public OperationType Type { get; set; }
+
+        public CRUDWrapper() 
+        { 
+            
+        }
+
+        public CRUDWrapper(T data, CRUDWrapper<T>.OperationType type)
+        {
+            Data = data;
+            Type = type;
+        }
+
+        public enum OperationType
+        {
+            Create,
+            Read,
+            Update,
+            Delete,
+            
+            Undefined
+        }
+    }
+
+    
+
+}
---
+++ GeorgiaTechLib/DataWrappers/CatWrapper.cs
@@ -0,0 +1,31 @@
+﻿using Webshop.Tools.DataWrappers;
+
+namespace DataWrappers
+{
+    public class CatWrapper<T>
+    {
+        public T Data { get; set; }
+        public OperationType Type { get; set; }
+
+        public CatWrapper()
+        {
+
+        }
+
+        public CatWrapper(T data, CatWrapper<T>.OperationType type)
+        {
+            Data = data;
+            Type = type;
+        }
+
+        public enum OperationType
+        {
+            Loaf,
+            Orange,
+            Vibin,
+            CatShapedVoid,
+            Cool,
+            Undefined
+        }
+    }
+}
